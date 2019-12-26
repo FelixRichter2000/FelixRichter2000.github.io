7e6766c75db913f066508c342a03c10c855d1b2e
Felix Richter <ric@viastore.com>
2019-12-26 16:43:51 +0100

    fixed replay bugs

--- js/match.js
+++ js/match.js
@@ -5,25 +5,52 @@ $replay_images = null;
 $replay_tds = null;
 TURN_INFORMATION = {};
 current_match_data_index = 0;
+playback_speed = 30;
 
 const playerImages = [
     ['../images/Filter1.svg', '../images/Encryptor1.svg', '../images/Destructor1.svg', '../images/Ping1.svg', '../images/Emp1.svg', '../images/Scrambler1.svg', '../images/Remove1.svg'],
     ['../images/Filter2.svg', '../images/Encryptor2.svg', '../images/Destructor2.svg', '../images/Ping2.svg', '../images/Emp2.svg', '../images/Scrambler2.svg', '../images/Remove2.svg']];
 const emptyImage = '../images/EmptyField.svg';
 
+createMatchTable();
 selected_match = 5472882;
 onMatchChanged();
+setMatchSpeed(playback_speed);
 
 // repeat with the interval 
-let showNextTimer = null;
+var showNextTimer = null;
+var play = false;
 function playMatch() {
-    showNextTimer = setInterval(() => showNext(), 1000 / 60);
+    play = true;
+    showMatchData(0);
+    current_match_data_index = 0;
+}
+
+function setMatchSpeed(speed) {
+    if (showNextTimer)
+        clearInterval(showNextTimer);
+    showNextTimer = setInterval(() => showNext(), 1000 / speed);
+}
+
+function showNext() {
+    if (!play) return;
+
+    for (var i = 0; i < 1; i++) {
+        updateToNextFrame(current_match_data_index);
+        current_match_data_index += 1;
+    }
+
+    updateReplayRangeSlider();
+}
+
+function updateReplayRangeSlider() {
+    $replay_range.val(current_match_data_index);
 }
 
 $replay_range.on('change', function () {
     showMatchData($(this).val());
     console.log($(this).val());
-    clearInterval(showNextTimer);
+    play = false;
 });
 
 $watch_on_terminal.on('click', function () {
@@ -33,17 +60,15 @@ $watch_on_terminal.on('click', function () {
 
 function onMatchChanged() {
     $('#match_id_label').html(selected_match);
+    play = false;
+    resetReplayTable();
+
     loadMatchData();
     current_match_data_index = 0;
+    updateReplayRangeSlider();
 }
 
-function showNext() {
-    for (var i = 0; i < 1; i++) {
-        updateToNextFrame(current_match_data_index);
-        current_match_data_index += 1;
-    }
-    $replay_range.val(current_match_data_index);
-}
+
 
 function loadMatchData() {
     $.ajax({
@@ -119,10 +144,14 @@ function setImg(location, path) {
     img.src = path;
 }
 
+function resetReplayTable() {
+    $replay_images.attr('src', emptyImage);
+}
+
 function showMatchData(i) {
     if (!(i in TURN_INFORMATION)) return;
 
-    $replay_images.attr('src', emptyImage);
+    resetReplayTable();
 
     var info = TURN_INFORMATION[i];
 
@@ -151,7 +180,7 @@ function showMatchData(i) {
 }
 
 //Create initial field table
-(function () {
+function createMatchTable() {
     for (var y = 0; y < 28; y++) {
         var new_row = $('<tr>');
         for (var x = 0; x < 28; x++) {
@@ -170,4 +199,4 @@ function showMatchData(i) {
     }
     $replay_images = $replay_table.find('img');
     $replay_tds = $replay_table.find('td');
-})();
+}
