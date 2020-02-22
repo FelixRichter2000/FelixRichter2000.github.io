9523491da9711e7d28224c6133a4b8d62aba5707
Felix Richter <ric@viastore.com>
2020-02-22 21:30:16 +0100

    added match_utils with tests

---
+++ js/match_utils.js
@@ -0,0 +1,12 @@
+﻿// JavaScript source code
+function generate_default_td_contents(src_options) {
+    let content = '<label class="quantity">';
+    for (var i = 0; i < src_options.length; i++) {
+        content += `<img src="${src_options[i]}">`;
+    }
+    return content;
+}
+
+module.exports = {
+    generate_default_td_contents
+};
---
+++ js/match_utils.test.js
@@ -0,0 +1,19 @@
+﻿// JavaScript source code
+const match_utils = require('./match_utils');
+
+test('Generate td contents with single source', () => {
+    expect(match_utils.generate_default_td_contents(['1']))
+        .toBe("<label class=\"quantity\"><img src=\"1\">");
+});
+
+test('Generate td contents with two sources (1, 2)', () => {
+    expect(match_utils.generate_default_td_contents(['1', '2']))
+        .toBe("<label class=\"quantity\"><img src=\"1\"><img src=\"2\">");
+});
+
+test('Generate td contents with two sources ("filter", "destructor", "encryptor")', () => {
+    expect(match_utils.generate_default_td_contents(["filter", "destructor", "encryptor"]))
+        .toBe("<label class=\"quantity\"><img src=\"filter\"><img src=\"destructor\"><img src=\"encryptor\">");
+});
+
+
