62959b566673ddb34c1e90bd60851ee72d7b0c1f
Felix Richter <ric@viastore.com>
2020-02-22 22:09:05 +0100

    added functions generate_settings, is_in_arena_bounds, generate_terminal_trs with unittests

--- js/match_utils.js
+++ js/match_utils.js
@@ -1,12 +1,42 @@
-﻿// JavaScript source code
+﻿
 function generate_default_td_contents(src_options) {
-    let content = '<label class="quantity">';
+    let content = '<label class="quantity"></label>';
     for (var i = 0; i < src_options.length; i++) {
         content += `<img src="${src_options[i]}">`;
     }
     return content;
 }
 
+function generate_settings(size) {
+    return {
+        size,
+        half_size: size / 2,
+    };
+}
+
+function is_in_arena_bounds(x, y, settings) {
+    return Math.abs(x - settings.half_size + .5) + Math.abs(y - settings.half_size + .5) < (settings.half_size + 1);
+}
+
+function generate_terminal_trs(settings, td_content) {
+    let trs = '';
+    for (var y = 0; y < settings.size; y++) {
+        trs += '<tr>';
+        for (var x = 0; x < settings.size; x++) {
+            trs += '<td>';
+            if (is_in_arena_bounds(x, y, settings)) {
+                trs += td_content;
+            }
+            trs += '</td>';
+        }
+        trs += '</tr>';
+    }
+    return trs;
+}
+
 module.exports = {
-    generate_default_td_contents
+    generate_default_td_contents,
+    generate_settings,
+    is_in_arena_bounds,
+    generate_terminal_trs
 };
--- js/match_utils.test.js
+++ js/match_utils.test.js
@@ -1,19 +1,128 @@
-﻿// JavaScript source code
-const match_utils = require('./match_utils');
+﻿const match_utils = require('./match_utils');
 
+//////////////////////////////////////////////
+//Test function generate_default_td_contents//
+//////////////////////////////////////////////
 test('Generate td contents with single source', () => {
     expect(match_utils.generate_default_td_contents(['1']))
-        .toBe("<label class=\"quantity\"><img src=\"1\">");
+        .toBe("<label class=\"quantity\"></label><img src=\"1\">");
 });
 
 test('Generate td contents with two sources (1, 2)', () => {
     expect(match_utils.generate_default_td_contents(['1', '2']))
-        .toBe("<label class=\"quantity\"><img src=\"1\"><img src=\"2\">");
+        .toBe("<label class=\"quantity\"></label><img src=\"1\"><img src=\"2\">");
 });
 
-test('Generate td contents with two sources ("filter", "destructor", "encryptor")', () => {
+test('Generate td contents with multiple real ("filter", "destructor", "encryptor")', () => {
     expect(match_utils.generate_default_td_contents(["filter", "destructor", "encryptor"]))
-        .toBe("<label class=\"quantity\"><img src=\"filter\"><img src=\"destructor\"><img src=\"encryptor\">");
+        .toBe("<label class=\"quantity\"></label><img src=\"filter\"><img src=\"destructor\"><img src=\"encryptor\">");
+});
+
+
+///////////////////////////////////
+//Test function generate_settings//
+///////////////////////////////////
+test('Generate setting size 4', () => {
+    expect(match_utils.generate_settings(4))
+        .toEqual({ "half_size": 2, "size": 4 });
+});
+
+test('Generate setting size 8', () => {
+    expect(match_utils.generate_settings(8))
+        .toEqual({ "half_size": 4, "size": 8 });
+});
+
+
+////////////////////////////////////
+//Test function is_in_arena_bounds//
+////////////////////////////////////
+test('Test is_in_arena_bounds expect false (1)', () => {
+    expect(match_utils.is_in_arena_bounds(0, 0, {size: 4, half_size: 2}))
+        .toBe(false);
+});
+
+test('Test is_in_arena_bounds expect false (2)', () => {
+    expect(match_utils.is_in_arena_bounds(0, 3, { size: 4, half_size: 2 }))
+        .toBe(false);
+});
+
+test('Test is_in_arena_bounds expect false (3)', () => {
+    expect(match_utils.is_in_arena_bounds(3, 3, { size: 4, half_size: 2 }))
+        .toBe(false);
+});
+
+test('Test is_in_arena_bounds expect false (4)', () => {
+    expect(match_utils.is_in_arena_bounds(3, 0, { size: 4, half_size: 2 }))
+        .toBe(false);
+});
+
+test('Test is_in_arena_bounds expect true (1)', () => {
+    expect(match_utils.is_in_arena_bounds(0, 1, { size: 4, half_size: 2 }))
+        .toBe(true);
+});
+
+test('Test is_in_arena_bounds expect true (2)', () => {
+    expect(match_utils.is_in_arena_bounds(0, 2, { size: 4, half_size: 2 }))
+        .toBe(true);
+});
+
+test('Test is_in_arena_bounds expect true (3)', () => {
+    expect(match_utils.is_in_arena_bounds(1, 0, { size: 4, half_size: 2 }))
+        .toBe(true);
+});
+
+test('Test is_in_arena_bounds expect true (4)', () => {
+    expect(match_utils.is_in_arena_bounds(1, 1, { size: 4, half_size: 2 }))
+        .toBe(true);
+});
+
+test('Test is_in_arena_bounds expect true (5)', () => {
+    expect(match_utils.is_in_arena_bounds(1, 2, { size: 4, half_size: 2 }))
+        .toBe(true);
+});
+
+test('Test is_in_arena_bounds expect true (6)', () => {
+    expect(match_utils.is_in_arena_bounds(1, 3, { size: 4, half_size: 2 }))
+        .toBe(true);
+});
+
+test('Test is_in_arena_bounds expect true (7)', () => {
+    expect(match_utils.is_in_arena_bounds(2, 0, { size: 4, half_size: 2 }))
+        .toBe(true);
+});
+
+test('Test is_in_arena_bounds expect true (8)', () => {
+    expect(match_utils.is_in_arena_bounds(2, 1, { size: 4, half_size: 2 }))
+        .toBe(true);
+});
+
+test('Test is_in_arena_bounds expect true (9)', () => {
+    expect(match_utils.is_in_arena_bounds(2, 2, { size: 4, half_size: 2 }))
+        .toBe(true);
+});
+
+test('Test is_in_arena_bounds expect true (10)', () => {
+    expect(match_utils.is_in_arena_bounds(2, 3, { size: 4, half_size: 2 }))
+        .toBe(true);
+});
+
+test('Test is_in_arena_bounds expect true (11)', () => {
+    expect(match_utils.is_in_arena_bounds(3, 1, { size: 4, half_size: 2 }))
+        .toBe(true);
+});
+
+test('Test is_in_arena_bounds expect true (12)', () => {
+    expect(match_utils.is_in_arena_bounds(3, 2, { size: 4, half_size: 2 }))
+        .toBe(true);
+});
+
+
+///////////////////////////////////////
+//Test function generate_terminal_trs//
+///////////////////////////////////////
+test('Generate generate_terminal_trs size 4', () => {
+    expect(match_utils.generate_terminal_trs({ size: 4, half_size: 2 }, '.'))
+        .toBe("<tr><td></td><td>.</td><td>.</td><td></td></tr><tr><td>.</td><td>.</td><td>.</td><td>.</td></tr><tr><td>.</td><td>.</td><td>.</td><td>.</td></tr><tr><td></td><td>.</td><td>.</td><td></td></tr>");
 });
 
 
