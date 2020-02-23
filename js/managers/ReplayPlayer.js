db707cc850a8a544cbd579c93af9cda967ebc692
Felix Richter <ric@viastore.com>
2020-02-23 12:38:26 +0100

    refactored replayPlayer init_table by using match_utils

--- js/managers/ReplayPlayer.js
+++ js/managers/ReplayPlayer.js
@@ -194,26 +194,11 @@
         return this.playerImages[player][type];
     }
     proto.init_table = function () {
-        for (var y = 0; y < 28; y++) {
-            var new_row = $('<tr>');
-            for (var x = 0; x < 28; x++) {
-                var new_td = $('<td>');
 
-                if (Math.abs(x - 13.5) + Math.abs(y - 13.5) < 15) {
-                    new_td
-                        .append($('<label>')
-                            .addClass('IULabel'));
-
-                    new_td.append($('<img>')
-                        .attr('id', (27 - y) * 1000 + x)
-                        .attr('src', this.emptyImage));
-                }
-                new_row
-                    .append(new_td);
-            }
-            this.$replay_table
-                .append(new_row);
-        }
+        let td_content = match_utils.generate_default_td_contents([this.emptyImage]);
+        let settings = match_utils.generate_settings(28);
+        let trs = match_utils.generate_terminal_trs(settings, td_content);
+        this.$replay_table.append(trs);
     }
 
     //visual stat updater
