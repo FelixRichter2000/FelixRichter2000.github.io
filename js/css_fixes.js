3bf3fb3109f56118720e6372d3ced1d77233ab4a
Felix Richter <ric@viastore.com>
2020-03-04 19:25:46 +0100

    reduced border space

--- js/css_fixes.js
+++ js/css_fixes.js
@@ -3,7 +3,7 @@
 document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`);
 document.documentElement.style.setProperty('--vw', `${window.innerWidth}px`);
 document.documentElement.style.setProperty('--min-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight))}px`);
-document.documentElement.style.setProperty('--tile-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight) / 34 - 1)}px`);
+document.documentElement.style.setProperty('--tile-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight) / 30 - 1)}px`);
 
 // We listen to the resize event
 window.addEventListener('resize', () => {
@@ -11,5 +11,5 @@ window.addEventListener('resize', () => {
     document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`);
     document.documentElement.style.setProperty('--vw', `${window.innerWidth}px`);
     document.documentElement.style.setProperty('--min-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight))}px`);
-    document.documentElement.style.setProperty('--tile-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight) / 34 - 1)}px`);
+    document.documentElement.style.setProperty('--tile-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight) / 30 - 1)}px`);
 });
