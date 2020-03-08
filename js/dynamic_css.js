10abc84675593a7fdef806ff7cc1eb546a80ec3e
Felix Richter <ric@viastore.com>
2020-03-08 09:09:11 +0100

    added graphics for dynamic health bars

--- js/dynamic_css.js
+++ js/dynamic_css.js
@@ -3,6 +3,7 @@
     document.documentElement.style.setProperty('--vw', `${window.innerWidth}px`);
     document.documentElement.style.setProperty('--min-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight))}px`);
     document.documentElement.style.setProperty('--tile-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight) / 28)}px`);
+    document.documentElement.style.setProperty('--bar-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight) / 24)}px`);
     document.documentElement.style.setProperty('--half-tile-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight) / 28) / 2}px`);
     document.documentElement.style.setProperty('--negative-half-tile-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight) / 28) / -2}px`);
 }
