d520c2f7a8b2fdc351cf6f76a1fe90f21751e8e2
Felix Richter <ric@viastore.com>
2020-03-08 10:16:40 +0100

    added more labels for bits and cores

--- js/dynamic_css.js
+++ js/dynamic_css.js
@@ -3,7 +3,9 @@
     document.documentElement.style.setProperty('--vw', `${window.innerWidth}px`);
     document.documentElement.style.setProperty('--min-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight))}px`);
     document.documentElement.style.setProperty('--tile-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight) / 28)}px`);
-    document.documentElement.style.setProperty('--bar-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight) / 24)}px`);
+    document.documentElement.style.setProperty('--bar-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight) / 25)}px`);
+    document.documentElement.style.setProperty('--smaller-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight) / 36)}px`);
+    document.documentElement.style.setProperty('--very-small-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight) / 42)}px`);
     document.documentElement.style.setProperty('--half-tile-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight) / 28) / 2}px`);
     document.documentElement.style.setProperty('--negative-half-tile-size', `${Math.floor(Math.min(window.innerWidth, window.innerHeight) / 28) / -2}px`);
 }
