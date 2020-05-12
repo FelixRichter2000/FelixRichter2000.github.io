976083feaf6dfc15d3bd72f882850a76e2fd93fe
FelixRichter2000 <felix-ri@gmx.de>
2020-05-12 22:32:10 +0200

    show multiple games on a single pair of algos in yellow

--- js/visualize_top_10.js
+++ js/visualize_top_10.js
@@ -97,9 +97,19 @@ function update_matches_in_table(result, top_algo_ids, set_algo_ids) {
 function make_winning_link(td, match_id) {
     td.innerHTML = `<a href="https://felixrichter2000.github.io/watch?id=${match_id}" target="_blank">W</a>`;
     td.classList.add("result-w");
+    if (td.classList.length != 1) {
+        td.classList.remove(...td.classList);
+        td.classList.add("result-u");
+        td.firstChild.innerHTML = "U";
+    }
 }
 
 function make_losing_link(td, match_id) {
     td.innerHTML = `<a href="https://felixrichter2000.github.io/watch?id=${match_id}" target="_blank">L</a>`;
     td.classList.add("result-l");
+    if (td.classList.length != 1) {
+        td.classList.remove(...td.classList);
+        td.classList.add("result-u");
+        td.firstChild.innerHTML = "U";
+    }
 }
