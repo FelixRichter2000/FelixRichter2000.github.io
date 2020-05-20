700667deba74a2147b72ccb826ff52f3a7dbc1cc
Felix Richter <felix-ri@gmx.de>
2020-05-20 13:46:13 +0200

    moved mouse move listener

--- js/dynamic_css.js
+++ js/dynamic_css.js
@@ -20,15 +20,5 @@ window.addEventListener('orientationchange', () => {
     update_custome_css_variables();
 });
 
-window.addEventListener('mousemove', (e) => {
-    let tile_size = Math.min(window.innerWidth, window.innerHeight) / 28;
 
-    const round = (pixel) => Math.round((pixel - tile_size / 2) / tile_size);
-
-    let x = round(e.clientX);
-    let y = 27 - round(e.clientY);
-
-    if (typeof viewer != 'undefined')
-        viewer.show_field_info(x, y);
-});
 
--- js/replay_player.js
+++ js/replay_player.js
@@ -283,7 +283,18 @@
         document.body.removeChild(el);
     }
 
+    //mouse move listener
+    window.addEventListener('mousemove', (e) => {
+        let tile_size = Math.min(window.innerWidth, window.innerHeight) / 28;
 
+        const round = (pixel) => Math.round((pixel - tile_size / 2) / tile_size);
+
+        let x = round(e.clientX);
+        let y = 27 - round(e.clientY);
+
+        if (typeof viewer != 'undefined')
+            viewer.show_field_info(x, y);
+    });
 
     // Keyboard Control Config
     let keybord_controls = [
