bbec9f35c1283c71d9bc27a413ce09aecba01ee5
Felix Richter <felix-ri@gmx.de>
2020-07-12 19:49:45 +0200

    added more features with tests to controller

--- js/utils/match/controller.js
+++ js/utils/match/controller.js
@@ -2,23 +2,62 @@ class Controller {
     constructor(matchViewer) {
         this.matchViewer = matchViewer;
         this.replayData = [];
+        this.frame = 0;
     }
 
     set_replay_data(replayData) {
         this.replayData = replayData;
     }
 
-    show_frame(frame) {
+    set_frame(frame) {
         if (this._is_frame_valid(frame))
             return this._show_frame_using_matchViewer(frame);
     }
 
+    next_frame() {
+        this.set_frame(this.frame + 1);
+    }
+
+    previous_frame() {
+        this.set_frame(this.frame - 1);
+    }
+
+    next_turn() {
+        this.set_frame(this._get_next_turn());
+    }
+
+    previous_turn() {
+        this.set_frame(this._get_previous_turn());
+    }
+
+    do_action(function_name, parameter) {
+        if (this[function_name])
+            this[function_name](parameter);
+    }
+
     _show_frame_using_matchViewer(frame) {
+        this.frame = frame;
         return this.matchViewer.show_data(this.replayData[frame]);
     }
 
     _is_frame_valid(frame) {
         return frame >= 0 && frame < this.replayData.length;
+    }
+
+    _get_next_turn(frame = this.frame) {
+        frame++;
+        while (frame < this.replayData.length && this.replayData[frame].turnInfo[0] !== 0) {
+            frame++;
+        }
+        return frame + 1;
+    }
+
+    _get_previous_turn(frame = this.frame) {
+        frame -= 2;
+        while (frame > 0 && this.replayData[frame].turnInfo[0] !== 0) {
+            frame--;
+        }
+        return frame + 1;
     }
 }
 
--- js/utils/match/controller.test.js
+++ js/utils/match/controller.test.js
@@ -2,6 +2,15 @@ const Controller = require('./controller');
 const MatchViewer = require('./match_viewer');
 jest.mock('./match_viewer');
 const mockMatchViewer = new MatchViewer();
+const mockReplayData = [
+    { frame: 0, turnInfo: [0] },
+    { frame: 1, turnInfo: [1] },
+    { frame: 2, turnInfo: [0] },
+    { frame: 3, turnInfo: [1] },
+    { frame: 4, turnInfo: [1] },
+    { frame: 5, turnInfo: [0] },
+    { frame: 6, turnInfo: [1] },
+]
 
 afterEach(() => {
     jest.clearAllMocks();
@@ -30,28 +39,28 @@ describe('Controller tests', function() {
 
     test('don`t throw exception when MatchViewer.show_data is called and replayData is not set', () => {
         const controller = new Controller(mockMatchViewer);
-        controller.show_frame(0);
+        controller.set_frame(0);
         expect(mockMatchViewer.show_data).not.toHaveBeenCalled();
     });
 
     test('don`t call MatchViewer.show_data when show_frame (-1) is called', () => {
         const controller = new Controller(mockMatchViewer);
         controller.set_replay_data([10, 11, 12]);
-        controller.show_frame(-1);
+        controller.set_frame(-1);
         expect(mockMatchViewer.show_data).not.toHaveBeenCalled();
     });
 
     test('don`t call MatchViewer.show_data when show_frame with bigger than highest index is called', () => {
         const controller = new Controller(mockMatchViewer);
         controller.set_replay_data([10, 11, 12]);
-        controller.show_frame(3);
+        controller.set_frame(3);
         expect(mockMatchViewer.show_data).not.toHaveBeenCalled();
     });
 
     test('call MatchViewer.show_data with correct data when show_frame (0) is called', () => {
         const controller = new Controller(mockMatchViewer);
         controller.set_replay_data([10, 11, 12]);
-        controller.show_frame(0);
+        controller.set_frame(0);
         expect(mockMatchViewer.show_data).toHaveBeenCalledWith(10);
         expect(mockMatchViewer.show_data).not.toHaveBeenCalledWith(11);
         expect(mockMatchViewer.show_data).not.toHaveBeenCalledWith(12);
@@ -60,9 +69,198 @@ describe('Controller tests', function() {
     test('call MatchViewer.show_data with correct data when show_frame (1) is called', () => {
         const controller = new Controller(mockMatchViewer);
         controller.set_replay_data([10, 11, 12]);
-        controller.show_frame(1);
+        controller.set_frame(1);
         expect(mockMatchViewer.show_data).not.toHaveBeenCalledWith(10);
         expect(mockMatchViewer.show_data).toHaveBeenCalledWith(11);
         expect(mockMatchViewer.show_data).not.toHaveBeenCalledWith(12);
+    });
+});
+
+describe('test do_action with function that does not exist', () => {
+    test('should not do anything', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.do_action('non_existing_function');
+    });
+});
+
+describe('test do_action set_frame', () => {
+    test('set_frame with frame_number when replay_data is not set', () => {
+        const controller = new Controller(mockMatchViewer);
+
+        controller.do_action('set_frame', 0);
+        expect(mockMatchViewer.show_data).not.toHaveBeenCalled();
+    });
+
+    test('set_frame with frame_number = 0', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data([10, 20, 30]);
+        controller.do_action('set_frame', 0);
+        expect(mockMatchViewer.show_data).toHaveBeenCalledWith(10);
+    });
+
+    test('set_frame with frame_number = 2', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data([10, 20, 30]);
+        controller.do_action('set_frame', 2);
+        expect(mockMatchViewer.show_data).toHaveBeenCalledWith(30);
+    });
+
+    test('set_frame with frame_number = -1', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data([10, 20, 30]);
+        controller.do_action('set_frame', -1);
+        expect(mockMatchViewer.show_data).not.toHaveBeenCalledWith(10);
+    });
+
+    test('set_frame with frame_number = 5', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data([10, 20, 30]);
+        controller.do_action('set_frame', 5);
+        expect(mockMatchViewer.show_data).not.toHaveBeenCalledWith(10);
+    });
+});
+
+describe('test do_action next_frame', () => {
+    test('next_frame when replay_data is not set', () => {
+        const controller = new Controller(mockMatchViewer);
+
+        controller.do_action('next_frame');
+        expect(mockMatchViewer.show_data).not.toHaveBeenCalled();
+    });
+
+    test('next_frame: show 2nd frame', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data([10, 20, 30]);
+        controller.do_action('next_frame');
+        expect(mockMatchViewer.show_data).toHaveBeenCalledWith(20);
+    });
+
+    test('next_frame twice: show 2nd and then 3rd frame', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data([10, 20, 30]);
+        controller.do_action('next_frame');
+        controller.do_action('next_frame');
+        expect(mockMatchViewer.show_data).toHaveBeenCalledWith(20);
+        expect(mockMatchViewer.show_data).toHaveBeenCalledWith(30);
+    });
+
+    test('next_frame: calling it to often makes it do nothing', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data([10, 20, 30]);
+        controller.do_action('next_frame');
+        controller.do_action('next_frame');
+        controller.do_action('next_frame');
+        controller.do_action('next_frame');
+        expect(mockMatchViewer.show_data).toHaveBeenCalledTimes(2);
+    });
+});
+
+describe('test do_action previous_frame', () => {
+    test('previous_frame when replay_data is not set', () => {
+        const controller = new Controller(mockMatchViewer);
+
+        controller.do_action('previous_frame');
+        expect(mockMatchViewer.show_data).not.toHaveBeenCalled();
+    });
+
+    test('previous_frame: with data set should still not do anything', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data([10, 20, 30]);
+        controller.do_action('previous_frame');
+        expect(mockMatchViewer.show_data).not.toHaveBeenCalledWith(20);
+    });
+
+    test('previous_frame: should work now', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data([10, 20, 30]);
+        controller.do_action('set_frame', 2);
+        controller.do_action('previous_frame');
+        expect(mockMatchViewer.show_data).toHaveBeenCalledWith(30);
+        expect(mockMatchViewer.show_data).toHaveBeenCalledWith(20);
+    });
+
+    test('combination of multiple next_frame and previous_frame calls', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data([10, 20, 30]);
+        controller.do_action('next_frame'); //1
+        controller.do_action('next_frame'); //2
+        controller.do_action('next_frame'); //does nothing
+        controller.do_action('next_frame'); //does nothing
+        controller.do_action('previous_frame'); //3
+        controller.do_action('previous_frame'); //4
+        controller.do_action('previous_frame'); //does nothing
+        controller.do_action('previous_frame'); //does nothing
+        controller.do_action('next_frame'); //5
+        expect(mockMatchViewer.show_data).toHaveBeenCalledTimes(5);
+    });
+});
+
+describe('test do_action next_turn', () => {
+    test('should not show next turn if it`s already the last turn', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data(mockReplayData);
+        controller.set_frame(6);
+        jest.clearAllMocks();
+        controller.do_action('next_turn');
+        expect(mockMatchViewer.show_data).toHaveBeenCalledTimes(0);
+    });
+
+    test('should find and show next turn', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data(mockReplayData);
+        controller.do_action('next_turn');
+        expect(mockMatchViewer.show_data).toHaveBeenCalledWith({ frame: 3, turnInfo: [1] });
+    });
+
+    test('should find and show next turn with multiple steps in between', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data(mockReplayData);
+        controller.set_frame(3);
+        controller.do_action('next_turn');
+        expect(mockMatchViewer.show_data).toHaveBeenCalledWith({ frame: 6, turnInfo: [1] });
+    });
+
+    test('should find and show next turn with multiple steps in between from turn frame -1', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data(mockReplayData);
+        controller.set_frame(2);
+        controller.do_action('next_turn');
+        expect(mockMatchViewer.show_data).toHaveBeenCalledWith({ frame: 6, turnInfo: [1] });
+    });
+});
+
+describe('test do_action previous_turn', () => {
+    test('should not show previous turn if it`s already the first turn', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data(mockReplayData);
+        controller.do_action('previous_turn');
+        expect(mockMatchViewer.show_data).toHaveBeenCalledTimes(0);
+    });
+
+    test('should find and show previous turn', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data(mockReplayData);
+        controller.set_frame(3);
+        jest.clearAllMocks();
+        controller.do_action('previous_turn');
+        expect(mockMatchViewer.show_data).toHaveBeenCalledWith({ frame: 1, turnInfo: [1] });
+    });
+
+    test('should find and show previous turn from turn_frame -1', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data(mockReplayData);
+        controller.set_frame(2);
+        jest.clearAllMocks();
+        controller.do_action('previous_turn');
+        expect(mockMatchViewer.show_data).toHaveBeenCalledWith({ frame: 1, turnInfo: [1] });
+    });
+
+    test('should find and show previous turn with multiple steps', () => {
+        const controller = new Controller(mockMatchViewer);
+        controller.set_replay_data(mockReplayData);
+        controller.set_frame(6);
+        jest.clearAllMocks();
+        controller.do_action('previous_turn');
+        expect(mockMatchViewer.show_data).toHaveBeenCalledWith({ frame: 3, turnInfo: [1] });
     });
 });
