9a4b6f430a96130c5063a81f611230add5a3fce0
Felix Richter <felix-ri@gmx.de>
2020-07-12 12:19:23 +0200

    added user_data_downloader

---
+++ js/utils/match/user_data_downloader.js
@@ -0,0 +1,19 @@
+class UserDataDownloader {
+    constructor(fetch_json = window.fetch_json) {
+        this.fetch_json = fetch_json;
+    }
+
+    async download(match_id) {
+        let request = `https://terminal.c1games.com/api/game/match/${match_id}/algos`;
+        await this.fetch_json(request).then(result => this.handle_result(result));
+        return this;
+    }
+
+    handle_result(result) {
+        this.algos = JSON.parse(result).data.algos;
+    }
+}
+
+if (typeof process !== 'undefined') {
+    module.exports = UserDataDownloader;
+}
---
+++ js/utils/match/user_data_downloader.test.js
@@ -0,0 +1,22 @@
+const UserDataDownloader = require('./user_data_downloader');
+let mock_fetch_json = jest.fn();
+mock_fetch_json.mockImplementation(() => new Promise(resolve => resolve('{"data": {"algos": [1, 2]}}')));
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+describe('UserDataDownloader tests', function() {
+    test('create UserDataDownloader', () => {
+        new UserDataDownloader(mock_fetch_json);
+    });
+
+    test('download replay config and replay are getting set', () => {
+        return new UserDataDownloader(mock_fetch_json)
+            .download(123456)
+            .then((result) => {
+                expect(mock_fetch_json).toHaveBeenCalledWith('https://terminal.c1games.com/api/game/match/123456/algos');
+                expect(result.algos).toEqual([1, 2]);
+            });
+    });
+});
