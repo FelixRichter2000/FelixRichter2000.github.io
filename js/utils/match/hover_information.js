3132b040f6994a02ee2337dfc848614e2485ffc0
Felix Richter <felix-ri@gmx.de>
2020-07-18 09:08:52 +0200

    added hover_information

---
+++ js/utils/match/hover_information.js
@@ -0,0 +1,27 @@
+class HoverInformation {
+    constructor(matchViewer, flatMatchViewer, configTools) {
+        this.matchViewer = matchViewer;
+        this.flatMatchViewer = flatMatchViewer;
+        this.configTools = configTools;
+        this.last_location = [0, 0];
+    }
+
+    show_field_info(x, y) {
+        this.last_location = [x, y];
+        this.update_hover();
+    }
+
+    update_hover() {
+        let health = this.matchViewer.get_value_at(this.last_location, 12);
+        let unit_type = this.matchViewer.get_value_at(this.last_location, 14);
+        let upgraded = this.matchViewer.get_value_at(this.last_location, 10);
+
+        let range = this.configTools.getRange(unit_type, upgraded);
+
+        this.flatMatchViewer.show_data({ location: this.last_location, range });
+    }
+}
+
+
+if (typeof process !== 'undefined')
+    module.exports = HoverInformation;
---
+++ js/utils/match/hover_information.test.js
@@ -0,0 +1,67 @@
+const HoverInformation = require("./hover_information");
+const MatchViewer = require('./match_viewer');
+jest.mock('./match_viewer');
+const mockMatchViewer = new MatchViewer();
+const mockFlatMatchViewer = new MatchViewer();
+const ConfigTools = require('../match/config_tools');
+jest.mock('../match/config_tools');
+const mockConfigTools = new ConfigTools();
+
+const HEALTH = 12;
+const UNIT_TYPE = 14;
+const UPGRADED = 10;
+
+mockMatchViewer.get_value_at.mockImplementation(() => 'returnValue');
+mockConfigTools.getRange.mockImplementation(() => 'returnedRange');
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+describe('test hover_information', () => {
+    test('create hover_information', () => {
+        new HoverInformation(mockMatchViewer, mockConfigTools);
+    });
+
+    test('show_field_info event', () => {
+        const hoverInformation = new HoverInformation(mockMatchViewer, mockFlatMatchViewer, mockConfigTools);
+        hoverInformation.show_field_info(0, 0);
+
+        expect(mockMatchViewer.get_value_at).toHaveBeenCalledWith([0, 0], HEALTH);
+        expect(mockMatchViewer.get_value_at).toHaveBeenCalledWith([0, 0], UNIT_TYPE);
+        expect(mockMatchViewer.get_value_at).toHaveBeenCalledWith([0, 0], UPGRADED);
+
+        expect(mockConfigTools.getRange).toHaveBeenCalledWith('returnValue', 'returnValue');
+
+        expect(mockFlatMatchViewer.show_data).toHaveBeenCalledWith({ location: [0, 0], range: 'returnedRange' });
+    });
+
+    test('update event first uses default location', () => {
+        const hoverInformation = new HoverInformation(mockMatchViewer, mockFlatMatchViewer, mockConfigTools);
+        hoverInformation.update_hover();
+
+        expect(mockMatchViewer.get_value_at).toHaveBeenCalledWith([0, 0], HEALTH);
+        expect(mockMatchViewer.get_value_at).toHaveBeenCalledWith([0, 0], UNIT_TYPE);
+        expect(mockMatchViewer.get_value_at).toHaveBeenCalledWith([0, 0], UPGRADED);
+
+        expect(mockConfigTools.getRange).toHaveBeenCalledWith('returnValue', 'returnValue');
+
+        expect(mockFlatMatchViewer.show_data).toHaveBeenCalledWith({ location: [0, 0], range: 'returnedRange' });
+    });
+
+    test('update event', () => {
+        const hoverInformation = new HoverInformation(mockMatchViewer, mockFlatMatchViewer, mockConfigTools);
+        hoverInformation.show_field_info(2, 3);
+        jest.clearAllMocks();
+
+        hoverInformation.update_hover();
+
+        expect(mockMatchViewer.get_value_at).toHaveBeenCalledWith([2, 3], HEALTH);
+        expect(mockMatchViewer.get_value_at).toHaveBeenCalledWith([2, 3], UNIT_TYPE);
+        expect(mockMatchViewer.get_value_at).toHaveBeenCalledWith([2, 3], UPGRADED);
+
+        expect(mockConfigTools.getRange).toHaveBeenCalledWith('returnValue', 'returnValue');
+
+        expect(mockFlatMatchViewer.show_data).toHaveBeenCalledWith({ location: [2, 3], range: 'returnedRange' });
+    });
+});
