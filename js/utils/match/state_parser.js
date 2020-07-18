e975c5a4b811cff743adf2bc227cccf3f933ef4f
Felix Richter <felix-ri@gmx.de>
2020-07-18 13:45:17 +0200

    added StateParser

---
+++ js/utils/match/state_parser.js
@@ -0,0 +1,43 @@
+class StateParser {
+
+    constructor(actionEventSystem) {
+        this.actionEventSystem = actionEventSystem;
+        this.stat_names = ['health', 'cores', 'bits', 'ms'];
+        this.turn_names = ['turn_type', 'turn_number', 'action_phase_frame_number', 'total_frame_number'];
+    }
+
+    update_frame_data(data) {
+        let stats = [data.p1Stats, data.p2Stats];
+        let turn_values = data.turnInfo;
+
+        let transformed = this._createObjectWithStats(stats);
+        transformed = this._addTurnProperties(transformed, turn_values);
+
+        this.actionEventSystem.release_event('update_view', transformed);
+    }
+
+    _addTurnProperties(transformed, turn_values) {
+        transformed = this.turn_names
+            .reduce((a, turn_name, index) => ({...a, [turn_name]: turn_values[index] }), transformed);
+        return transformed;
+    }
+
+    _createObjectWithStats(stats) {
+        return this.stat_names
+            .map(this._getValuesFromGivenStatName(stats))
+            .reduce(this._addStatProperty(), {});
+    }
+
+    _addStatProperty() {
+        return (a, values, index) => ({...a, [this.stat_names[index]]: values });
+    }
+
+    _getValuesFromGivenStatName(stats) {
+        return (_, index) => stats
+            .reduce((a, stat) => [...a, stat[index]], []);
+    }
+}
+
+
+if (typeof process !== 'undefined')
+    module.exports = StateParser;
---
+++ js/utils/match/state_parser.test.js
@@ -0,0 +1,35 @@
+const StateParser = require("./state_parser");
+const ActionEventSystem = require('../general/action_event_system');
+jest.mock('../general/action_event_system');
+const mockActionEventSystem = new ActionEventSystem();
+
+describe('test StateParser', () => {
+    test('create StateParser', () => {
+        new StateParser(mockActionEventSystem);
+    });
+
+    test('update_data should parse the data and send an update_view event with the parsed data', () => {
+        let stateParser = new StateParser(mockActionEventSystem);
+
+        const input_data = {
+            p1Stats: ['health1', 'cores1', 'bits1', 'ms1'],
+            p2Stats: ['health2', 'cores2', 'bits2', 'ms2'],
+            turnInfo: ['turnType', 'turnNumber', 'actionPhaseFrameNumber', 'totalFrameNumber']
+        }
+
+        const expected_output_data = {
+            health: ['health1', 'health2'],
+            cores: ['cores1', 'cores2'],
+            bits: ['bits1', 'bits2'],
+            ms: ['ms1', 'ms2'],
+            turn_type: 'turnType',
+            turn_number: 'turnNumber',
+            action_phase_frame_number: 'actionPhaseFrameNumber',
+            total_frame_number: 'totalFrameNumber'
+        }
+
+        stateParser.update_frame_data(input_data);
+
+        expect(mockActionEventSystem.release_event).toHaveBeenCalledWith('update_view', expected_output_data);
+    });
+});
