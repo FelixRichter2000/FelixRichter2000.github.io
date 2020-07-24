b55274547756d26b41ab34f7c7e5000eeb8f7fdf
Felix Richter <felix-ri@gmx.de>
2020-07-24 19:52:50 +0200

    finished socket

--- js/utils/match/socket.js
+++ js/utils/match/socket.js
@@ -1,29 +1,96 @@
 class Socket {
+    prefixes = ['m!', 'm!', 'm"', 'm"'];
+
     constructor(init_string) {
-        let self = this;
         this.socket = new WebSocket("wss://playground.c1games.com/");
-        this.socket.onopen = function() {
-            self.socket.send(init_string);
-        };
-        this.socket.onmessage = function(message) {
-            if (message.data[0] === 'm') {
-                let parsed = JSON.parse(message.data.substring(2));
-                if (parsed.p2Units) {
-                    self.flag = true;
-                    self._try_submit();
-                }
-            }
-        };
+        this.socket.onopen = (_ => this.socket.send(init_string)).bind(this);
+        this.socket.onmessage = (message => this._handle_message(message)).bind(this);
+        this.all_messages = [];
     }
 
-    submit_actions(actions) {
+    async submit_actions(actions) {
         this.actions = actions;
-        this._try_submit();
+        this._submit_if_possible();
+        return this._create_promise();
     }
 
-    _try_submit() {
-        if (this.flag && this.actions)
-            this.socket.send(this.actions);
+    _create_promise() {
+        return new Promise((resolve) => this.resolve = resolve);
+    }
+
+    _handle_message(message) {
+        if (this._is_message_relevant(message))
+            this._use_message(message);
+    }
+
+    _use_message(message) {
+        let parsed_message = this._parse_message(message);
+
+        if (!this.has_submitted)
+            this._handle_early_message(parsed_message);
+        else
+            this._handle_turn_message(parsed_message);
+    }
+
+    _handle_early_message(parsed_message) {
+        if (this._is_message_starting_state(parsed_message))
+            this._enable_any_try_to_submit();
+    }
+
+    _enable_any_try_to_submit() {
+        this.submit_enabled = true;
+        this._submit_if_possible();
+    }
+
+    _is_message_starting_state(parsed_message) {
+        return parsed_message.p2Units && parsed_message.turnInfo[0] === 0;
+    }
+
+    _handle_turn_message(parsed_message) {
+        this._add_to_all_messages(parsed_message);
+        if (this._is_end_of_turn(parsed_message))
+            this._resolve_promise();
+    }
+
+    _parse_message(message) {
+        return JSON.parse(message.data.substring(2));
+    }
+
+    _add_to_all_messages(parsed) {
+        this.all_messages.push(parsed);
+    }
+
+    _is_message_relevant(message) {
+        return !this.has_resolved && message.data[0] === 'm';
+    }
+
+    _resolve_promise() {
+        this.resolve(this.all_messages);
+        this.has_resolved = true;
+        this.socket.close();
+    }
+
+    _is_end_of_turn(parsed) {
+        return parsed.turnInfo[2] == -1;
+    }
+
+    _submit_if_possible() {
+        if (this._can_submit())
+            this._submit();
+    }
+
+    _submit() {
+        this._create_messages()
+            .forEach(e => this.socket.send(e));
+        this.has_submitted = true;
+    }
+
+    _create_messages() {
+        return this.prefixes.map((prefix, i) => prefix + JSON.stringify(this.actions[i]));
+    }
+
+    _can_submit() {
+        return this.submit_enabled && this.actions;
     }
 }
 
--- js/utils/match/socket.test.js
+++ js/utils/match/socket.test.js
@@ -1,6 +1,21 @@
 const Socket = require("./socket");
 const { WS } = require("jest-websocket-mock");
 let terminalServer;
+const testing_actions = [
+    ['p1_firewalls'],
+    ['p1_units'],
+    ['p2_firewalls'],
+    ['p2_units']
+];
+const action_messages = [
+    'm!["p1_firewalls"]',
+    'm!["p1_units"]',
+    'm"["p2_firewalls"]',
+    'm"["p2_units"]'
+];
+
+jest.setTimeout(20);
+jest.useFakeTimers();
 
 beforeEach(() => {
     terminalServer = new WS("wss://playground.c1games.com/");
@@ -34,16 +49,20 @@ describe('test ws-mock', () => {
     });
 });
 
-describe('test socket', () => {
+describe('create socket', () => {
     it('should create a WebSocket to the terminalServer and send the init string', async() => {
         new Socket("init_string");
+        jest.runTimersToTime(10);
         await terminalServer.nextMessage;
         expect(terminalServer.messages).toEqual(["init_string"]);
     });
+});
 
+describe('socket.submit_turn', () => {
     it('should not submit turn before the starting state got returned', async() => {
         let socket = new Socket("init_string");
         socket.submit_actions([]);
+        jest.runTimersToTime(10);
         await terminalServer.nextMessage;
         expect(terminalServer.messages).toEqual(["init_string"]);
     });
@@ -53,10 +72,10 @@ describe('test socket', () => {
         terminalServer.send('a [{"make_move_1":33,"make_move_2":34},{}]');
         terminalServer.send('m {"debug":"lots of data"}');
         terminalServer.send('m {"p2Units":[[],[],[],[],[],[],[],[]],"turnInfo":[0,12,-1,0]}');
-        await terminalServer.nextMessage;
-        socket.submit_actions([1, 2, 3, 4]);
-        await terminalServer.nextMessage;
-        expect(terminalServer.messages).toEqual(["init_string", [1, 2, 3, 4]]);
+        jest.runTimersToTime(10);
+        socket.submit_actions(testing_actions);
+        jest.runTimersToTime(10);
+        expect(terminalServer.messages).toEqual(["init_string", ...action_messages]);
     });
 
     it('should not submit turn when no message containing p2Units arrived before', async() => {
@@ -64,30 +83,70 @@ describe('test socket', () => {
         terminalServer.send('a [{"make_move_1":33,"make_move_2":34},{}]');
         terminalServer.send('m {"debug":"lots of data"}');
         terminalServer.send('m {"p6Units":[[],[],[],[],[],[],[],[]],"turnInfo":[0,12,-1,0]}');
-        await terminalServer.nextMessage;
-        socket.submit_actions([1, 2, 3, 4]);
+        jest.runTimersToTime(10);
+        socket.submit_actions(testing_actions);
+        jest.runTimersToTime(10);
         expect(terminalServer.messages).toEqual(["init_string"]);
     });
 
     it('should wait with submit turn until message containing p2Units arrived', async() => {
         let socket = new Socket("init_string");
-        await terminalServer.nextMessage;
-        socket.submit_actions([1, 2, 3, 4]);
+        jest.runTimersToTime(10);
+        socket.submit_actions(testing_actions);
         terminalServer.send('m {"p2Units":[[],[],[],[],[],[],[],[]],"turnInfo":[0,12,-1,0]}');
-        await terminalServer.nextMessage;
-        expect(terminalServer.messages).toEqual(["init_string", [1, 2, 3, 4]]);
+        jest.runTimersToTime(10);
+        expect(terminalServer.messages).toEqual(["init_string", ...action_messages]);
     });
 
-    /*
-n[{"game_to_client":0,"player_1_err":1,"player_2_err":2,"game_over":3,"notification":4},{"algo_zip":0},"%7B%22player_1%22%3A%22manual%22%2C%22player_2%22%3A%22manual%22%2C%22p1_use_old_config%22%3Afalse%2C%22p2_use_old_config%22%3Afalse%2C%22start_string%22%3A%22%7B%5C%22p2Units%5C%22%3A%5B%5B%5D%2C%5B%5D%2C%5B%5D%2C%5B%5D%2C%5B%5D%2C%5B%5D%2C%5B%5D%2C%5B%5D%5D%2C%5C%22turnInfo%5C%22%3A%5B0%2C12%2C-1%2C843%5D%2C%5C%22p1Stats%5C%22%3A%5B24%2C18%2C6.7%2C114%5D%2C%5C%22p1Units%5C%22%3A%5B%5B%5D%2C%5B%5D%2C%5B%5D%2C%5B%5D%2C%5B%5D%2C%5B%5D%2C%5B%5D%2C%5B%5D%5D%2C%5C%22p2Stats%5C%22%3A%5B21%2C5%2C14.2%2C14%5D%2C%5C%22events%5C%22%3A%7B%5C%22selfDestruct%5C%22%3A%5B%5D%2C%5C%22breach%5C%22%3A%5B%5D%2C%5C%22damage%5C%22%3A%5B%5D%2C%5C%22shield%5C%22%3A%5B%5D%2C%5C%22move%5C%22%3A%5B%5D%2C%5C%22spawn%5C%22%3A%5B%5D%2C%5C%22death%5C%22%3A%5B%5D%2C%5C%22attack%5C%22%3A%5B%5D%2C%5C%22melee%5C%22%3A%5B%5D%7D%7D%22%2C%22match_name%22%3A%22%22%2C%22config_id%22%3Anull%2C%22config_type%22%3Anull%7D"]	1000	
-a[{"make_move_1":33,"make_move_2":34},{}]	41	
-m{"debug":{"printMapString":false,"printTStrings":false,"printActStrings":false,"printHitStrings":false,"printPlayerInputStrings":false,"printBotErrors":true,"printPlayerGetHitStrings":false},"unitInformation":[{"turnsRequiredToRemove":1,"refundPercentage":0.75,"cost1":1.0,"getHitRadius":0.01,"upgrade":{"startHealth":120.0},"unitCategory":0,"display":"Filter","startHealth":60.0,"shorthand":"FF"},{"refundPercentage":0.75,"cost1":4.0,"upgrade":{"shieldBonusPerY":0.3,"shieldRange":7.0,"shieldPerUnit":4.0},"shieldRange":3.5,"shieldPerUnit":3.0,"display":"Encryptor","shorthand":"EF","turnsRequiredToRemove":1,"shieldBonusPerY":0.0,"getHitRadius":0.01,"unitCategory":0,"startHealth":30.0,"shieldDecay":0.0},{"attackDamageWalker":5.0,"attackRange":2.5,"turnsRequiredToRemove":1,"refundPercentage":0.75,"cost1":2.0,"getHitRadius":0.01,"upgrade":{"attackDamageWalker":16.0,"attackRange":3.5,"cost1":4.0},"unitCategory":0,"display":"Destructor","startHealth":75.0,"attackDamageTower":0.0,"shorthand":"DF"},{"attackRange":3.5,"selfDestructDamageTower":15.0,"cost2":1.0,"metalForBreach":1.0,"display":"Ping","selfDestructStepsRequired":5,"shorthand":"PI","playerBreachDamage":1.0,"speed":1.0,"attackDamageWalker":2.0,"getHitRadius":0.01,"unitCategory":1,"selfDestructDamageWalker":15.0,"startHealth":15.0,"selfDestructRange":1.5,"attackDamageTower":2.0},{"attackRange":4.5,"selfDestructDamageTower":5.0,"cost2":3.0,"metalForBreach":1.0,"display":"EMP","selfDestructStepsRequired":5,"shorthand":"EI","playerBreachDamage":1.0,"speed":0.5,"attackDamageWalker":8.0,"getHitRadius":0.01,"unitCategory":1,"selfDestructDamageWalker":5.0,"startHealth":5.0,"selfDestructRange":1.5,"attackDamageTower":8.0},{"attackRange":4.5,"selfDestructDamageTower":40.0,"cost2":1.0,"metalForBreach":1.0,"display":"Scrambler","selfDestructStepsRequired":5,"shorthand":"SI","playerBreachDamage":1.0,"speed":0.25,"attackDamageWalker":20.0,"getHitRadius":0.01,"unitCategory":1,"selfDestructDamageWalker":40.0,"startHealth":40.0,"selfDestructRange":1.5,"attackDamageTower":0.0},{"display":"Remove","shorthand":"RM"},{"display":"Upgrade","shorthand":"UP"}],"timingAndReplay":{"playReplaySave":0,"waitTimeBotMax":40000,"waitTimeManual":1820000,"waitForever":false,"playWaitTimeBotSoft":10000,"waitTimeEndGame":3000,"waitTimeBotSoft":10000,"playWaitTimeBotMax":40000,"replaySave":0,"storeBotTimes":true,"waitTimeStartGame":3000},"resources":{"bitsPerRound":5.0,"coresPerRound":5.0,"startingBits":5.0,"turnIntervalForBitCapSchedule":10,"turnIntervalForBitSchedule":10,"bitRampBitCapGrowthRate":5.0,"bitDecayPerRound":0.25,"roundStartBitRamp":10,"bitGrowthRate":1.0,"startingHP":30.0,"startingCores":40.0,"maxBits":150.0},"seasonCompatibilityModeP2":5,"seasonCompatibilityModeP1":5}	2745	
-m{"p2Units":[[],[],[],[],[],[],[],[]],"turnInfo":[0,12,-1,0],"p1Stats":[24.0,18.0,6.7,0],"p1Units":[[],[],[],[],[],[],[],[]],"p2Stats":[21.0,5.0,14.2,0],"events":{"selfDestruct":[],"breach":[],"damage":[],"shield":[],"move":[],"spawn":[],"death":[],"attack":[],"melee":[]}}	275	
-m!"[]	5	
-m![["PI",4,9]]	14	
-m"[["FF",6,7]]	14	
-m"[]	4	
-m{"p2Units":[[[21,20,60.0,"1"]],[],[],[],[],[],[],[]],"turnInfo":[1,12,0,1],"p1Stats":[24.0,18.0,5.7,271],"p1Units":[[],[],[],[[4,10,15.0,"2"]],[],[],[],[]],"p2Stats":[21.0,4.0,14.2,271],"events":{"selfDestruct":[],"breach":[],"damage":[],"shield":[],"move":[[[4,9],[4,10],[0,0],3,"2",1]],"spawn":[[[21,20],0,"1",2],[[4,9],3,"2",1]],"death":[],"attack":[],"melee":[]}}	370	
-m{"p2Units":[[[21,20,60.0,"1"]],[],[],[]
-    */
+    it('should return an array of all replay frames (deserialized) in object form', async() => {
+        let socket = new Socket("init_string");
+        jest.runTimersToTime(10);
+        let retured_actions = socket.submit_actions(testing_actions);
+        terminalServer.send('m {"p2Units":[[],[],[],[],[],[],[],[]],"turnInfo":[0,12,-1,0]}');
+        jest.runTimersToTime(10);
+        terminalServer.send('m {"p2Units":[],"turnInfo":[1,12,0,0]}'); //frames 0, 1, 2, 3, ...
+        terminalServer.send('m {"p2Units":[],"turnInfo":[1,12,1,0]}');
+        terminalServer.send('m {"p2Units":[],"turnInfo":[1,12,2,0]}');
+        terminalServer.send('m {"p2Units":[],"turnInfo":[1,12,3,0]}');
+        terminalServer.send('m {"p2Units":[],"turnInfo":[0,13,-1,0]}'); //end_state -1
+        return retured_actions.then(a => expect(a).toEqual([{
+            "p2Units": [],
+            "turnInfo": [1, 12, 0, 0]
+        }, {
+            "p2Units": [],
+            "turnInfo": [1, 12, 1, 0]
+        }, {
+            "p2Units": [],
+            "turnInfo": [1, 12, 2, 0]
+        }, {
+            "p2Units": [],
+            "turnInfo": [1, 12, 3, 0]
+        }, {
+            "p2Units": [],
+            "turnInfo": [0, 13, -1, 0]
+        }]));
+    });
+
+    it('should return an array of all replay frames (deserialized) in object form when one message arrives that has a turnInfo[2] of -1', async() => {
+        let socket = new Socket("init_string");
+        jest.runTimersToTime(10);
+        let retured_actions = socket.submit_actions(testing_actions);
+        terminalServer.send('m {"p2Units":[[],[],[],[],[],[],[],[]],"turnInfo":[0,12,-1,0]}');
+        jest.runTimersToTime(10);
+        terminalServer.send('m {"p2Units":[],"turnInfo":[1,12,0,0]}'); //frames 0, 1, 2, 3, ...
+        terminalServer.send('m {"p2Units":[],"turnInfo":[1,12,1,0]}');
+        terminalServer.send('m {"p2Units":[],"turnInfo":[0,13,-1,0]}'); //end_state -1
+        terminalServer.send('m {"p2Units":[],"turnInfo":[1,12,2,0]}'); //ignore following...
+        terminalServer.send('m {"p2Units":[],"turnInfo":[1,12,3,0]}');
+        return retured_actions.then(a => expect(a).toEqual([{
+            "p2Units": [],
+            "turnInfo": [1, 12, 0, 0]
+        }, {
+            "p2Units": [],
+            "turnInfo": [1, 12, 1, 0]
+        }, {
+            "p2Units": [],
+            "turnInfo": [0, 13, -1, 0]
+        }]));
+    });
 });
