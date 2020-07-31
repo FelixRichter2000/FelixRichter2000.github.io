5b50ed3bb77ba8e830e876f3812ba6e0ef3babe0
Felix Richter <felix-ri@gmx.de>
2020-07-31 21:45:14 +0200

    added tests for scramblers

--- js/utils/match/change_detector.test.js
+++ js/utils/match/change_detector.test.js
@@ -575,4 +575,46 @@ describe('detect changes', () => {
             []
         ]);
     });
+
+    it('should return three additional scramblers at 1, 12', () => {
+        const game_state_after = {
+            p1Units: [
+                [],
+                [],
+                [],
+                [],
+                [],
+                [
+                    [1, 12, 60, '1'],
+                    [1, 12, 60, '2'],
+                    [1, 12, 60, '3'],
+                ],
+                [],
+                []
+            ],
+            p2Units: [
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                []
+            ]
+        };
+
+        let change_detector = new ChangeDetector();
+        let changes = change_detector.detect_changes(empty_game_state, game_state_after);
+        expect(changes).toEqual([
+            [],
+            [
+                ['SI', 1, 12],
+                ['SI', 1, 12],
+                ['SI', 1, 12],
+            ],
+            [],
+            []
+        ]);
+    });
 });
