41e06bab8f3f613b2688c5d29523322b9d6965c6
FelixRichter2000 <f.richter@viastore.com>
2020-12-26 10:09:09 +0100

    feat: add attacksManager

---
+++ js/utils/match/attacks_manager.js
@@ -0,0 +1,49 @@
+class AttacksManager {
+    constructor(actionEventSystem) {
+        this.actionEventSystem = actionEventSystem;
+        this.switched = false;
+        this.attack_index = 0;
+        this.attacks = [[[]], [[]]];
+    }
+
+    set_attacks(attacks) {
+        this.attacks = attacks;
+        this._release_set_actions_event();
+    }
+
+    _release_set_actions_event() {
+        this.actionEventSystem.release_event('set_actions', this.attacks[+this.switched][this.attack_index]);
+    }
+
+    set_actions(actions) {
+        this.attacks[+this.switched][this.attack_index] = JSON.parse(JSON.stringify(actions));
+    }
+
+    switch_view() {
+        this.switched = !this.switched;
+        this.attack_index = 0;
+        this._release_set_actions_event();
+    }
+
+    next_attack() {
+        if (this.attacks[+this.switched].length > this.attack_index + 1) {
+            this.attack_index += 1;
+            this._release_set_actions_event();
+        }
+    }
+
+    previous_attack() {
+        if (this.attack_index > 0) {
+            this.attack_index -= 1;
+            this._release_set_actions_event();
+        }
+    }
+
+    get_attacks() {
+        return this.attacks[+this.switched];
+    }
+}
+
+
+if (typeof process !== 'undefined')
+    module.exports = AttacksManager;
